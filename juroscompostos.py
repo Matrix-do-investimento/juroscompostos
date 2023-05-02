61336361e5cf0f433c5d6e83492887261825b860
Yuri Rodrigues Teixeira <71528635+yuri3gp@users.noreply.github.com>
2023-05-02 18:55:59 -0300

    mascaras

--- juroscompostos.py
+++ juroscompostos.py
@@ -2,27 +2,34 @@ import kivy
 from kivy.app import App
 from kivy.uix.gridlayout import GridLayout
 from kivy.uix.label import Label
-from kivy.uix.textinput import TextInput
 from kivy.uix.button import Button
+from kivy.uix.textinput import TextInput
+
+class NumberInput(TextInput):
+    def insert_text(self, substring, from_undo=False):
+        if substring.isnumeric() or substring == "." or substring == ",":
+            if substring == ",":
+                substring = "."
+            return super().insert_text(substring, from_undo=from_undo)
 
 class JurosCompostos(App):
     def build(self):
         layout = GridLayout(cols=2)
 
         layout.add_widget(Label(text="Valor Inicial:"))
-        self.valor_inicial = TextInput(multiline=False)
+        self.valor_inicial = NumberInput(multiline=False)
         layout.add_widget(self.valor_inicial)
 
         layout.add_widget(Label(text="Taxa de juros anual (%):"))
-        self.taxa = TextInput(multiline=False)
+        self.taxa = NumberInput(multiline=False)
         layout.add_widget(self.taxa)
 
         layout.add_widget(Label(text="Período de tempo (anos):"))
-        self.tempo = TextInput(multiline=False)
+        self.tempo = NumberInput(multiline=False)
         layout.add_widget(self.tempo)
 
         layout.add_widget(Label(text="Aporte mensal:"))
-        self.aporte_mensal = TextInput(multiline=False)
+        self.aporte_mensal = NumberInput(multiline=False)
         layout.add_widget(self.aporte_mensal)
 
         layout.add_widget(Label(text="Valor Investido:"))
@@ -50,10 +57,16 @@ class JurosCompostos(App):
 
         # Fórmula de juros compostos com aportes mensais: Vf = P * (1 + r/n) ^ (n*t) + m * [((1 + r/n) ^ (n*t) - 1) / (r/n)]
         n = 12 # número de vezes que o juros é aplicado por ano, para este exemplo usaremos mensal
-        vf = p * (1 + (r/100)/n) ** (n*t) + m * (((1 + (r/100)/n) ** (n*t) - 1) / ((r/100)/n))
-        juros = vf - p - m * (t * 12)
+        vf = p
+        investido = p
+        for i in range(int(t * n)):
+            investido += m
+            vf *= (1 + (r/100)/n)
+            vf += m
+
+        juros = vf - investido
         
-        self.valor_investido.text = "R$ {:.2f}".format(p)
+        self.valor_investido.text = "R$ {:.2f}".format(investido)
         self.valor_juros.text = "R$ {:.2f}".format(juros)
         self.valor_final.text = "R$ {:.2f}".format(vf)
 
